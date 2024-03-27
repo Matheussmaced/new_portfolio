977e45b91da955e971591318d48635622994e73a
Matheussmaced <mateusbyakuya3@gmail.com>
2024-03-27 19:24:16 -0300

    feat: creating profile component

---
+++ src/app/(home)/components/profile.tsx
@@ -0,0 +1,21 @@
+export default function Profile (){
+  return ( 
+    <>
+      <main>
+        <h1>Matheus Gregorio</h1>
+
+        <p> Desenvolvo experiências digitais impecáveis, cativantes e inclusivas.</p>
+
+        <div>
+          <span></span>
+          <p>SOBRE</p>
+        </div>
+
+        <div>
+          <span></span>
+          <p>PROJETOS</p>
+        </div>
+      </main>
+    </>
+   )
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,13 +4,14 @@ import { GlobalStyles } from "@/styles/global";
 import { defaultTheme } from "@/themes/default";
 
 import { ThemeProvider } from "styled-components";
+import Profile from "./components/profile";
 
 export default function Home() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
 
-      <h1>test</h1>
+      <Profile />
     </ThemeProvider>
   );
 }
