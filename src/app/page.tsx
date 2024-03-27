b92148c550ae6c698935b1bb6d5ab41826a0e3a3
Matheussmaced <mateusbyakuya3@gmail.com>
2024-03-27 11:47:11 -0300

    feat: styled-components config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { GlobalStyles } from "@/styles/global";
+import { defaultTheme } from "@/themes/default";
+
+import { ThemeProvider } from "styled-components";
+
 export default function Home() {
   return (
-    <main>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+
       <h1>test</h1>
-    </main>
+    </ThemeProvider>
   );
 }
